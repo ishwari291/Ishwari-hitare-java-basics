d444f5491d5ca051e8b3a6d9d4e3acc8ac440645
ishwari291 <hitareishwari@gmail.com>
2026-07-31 18:37:02 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        int fact = 1;
+        int i = 1;
+
+        while (i <= num) {
+            fact *= i;
+            i++;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
+}
