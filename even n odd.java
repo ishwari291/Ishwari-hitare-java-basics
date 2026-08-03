1136189d160bdbbcf912ec405147e4ca52c31599
ishwari291 <hitareishwari@gmail.com>
2026-08-03 19:21:02 +0530

    Create even n odd.java

---
+++ even n odd.java
@@ -0,0 +1,11 @@
+public class Evenodd  {
+public ststic void main(string[]args) {
+int number = 15;
+
+if (number % 2 == 0) { 
+          system.out.println(number + "is even.");
+} else {
+  system.out.println(number + " is odd.");
+  }
+ }
+}
